86fa84c3061f0222229c452f0e912450ee86856e
Jean Tovar <jeantovar@gmail.com>
2017-03-21 03:40:11 +0000

    Editar Articulos listo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  
   def new
     @article = Article.new
   end
@@ -12,12 +13,30 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-    
+  end
+  
+  def edit
+    @article= Article.find(params[:id])
+  end 
+  
+  def update
+    @article= Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Artculo actualizado satisfactoriamente"
+      redirect_to article_path(@article)
+      
+    else
+      render 'edit'
+    end
   end
   
   def show
     @article = Article.find(params[:id])
   end
+  
+  def index
+    @articles= Article.all  
+  end 
   
   private
   
