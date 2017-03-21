11fe56048340bca5b18f6dc6473efca138997349
Jean Tovar <jeantovar@gmail.com>
2017-03-21 18:43:47 +0000

    Agregando Partials a los formularios

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Articulo eliminado"
+    redirect_to articles_path
+  end
+  
   def index
     @articles= Article.all  
   end 
