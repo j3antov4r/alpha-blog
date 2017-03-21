666cfeecdda750160eb9f96a3c56f0707c088bec
Jean Tovar <jeantovar@gmail.com>
2017-03-21 19:31:13 +0000

    mejora en aricles_controller agregado before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy, :update] 
   
   def new
     @article = Article.new
@@ -20,7 +21,7 @@ class ArticlesController < ApplicationController
   end 
   
   def update
-    @article= Article.find(params[:id])
+    #@article= Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Artculo actualizado satisfactoriamente"
       redirect_to article_path(@article)
@@ -31,11 +32,11 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Articulo eliminado"
     redirect_to articles_path
@@ -46,6 +47,10 @@ class ArticlesController < ApplicationController
   end 
   
   private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
   
   def article_params
     params.require(:article).permit(:title, :description)
