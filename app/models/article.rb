555dec25a74648f9c9edfc0f9aeed3569355eef1
j3antov4r <jeantovar@gmail.com>
2017-04-29 20:50:03 -0400

    Styling List, Show, etc

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   #validaciones para BD
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 5, maximum: 120}
+  validates :description, presence: true, length: {minimum: 5, maximum: 500}
 end
