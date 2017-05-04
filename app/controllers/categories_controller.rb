fc3a798fa62a2d02bb8960567b690da42ce709a4
j3antov4r <jeantovar@gmail.com>
2017-05-03 20:50:08 -0400

    added admin restrictions for create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
 
+	before_action :require_admin , except: [:index, :show] 
+	before_action :set_category, only: [:show]
+
 	def index
 		@categories = Category.paginate( page: params[:page], per_page: 5 )
 	end
@@ -27,5 +30,16 @@ class CategoriesController < ApplicationController
 	def category_params
 		params.require(:category).permit(:name)
 	end
+
+	def require_admin
+		if !logged_in? || (logged_in? && !current_user.admin?)
+			flash[:danger] = "Only admins can perform that action"
+			redirect_to categories_path 
+		end
+	end
+
+	def set_category
+  	@category = Category.find( params[:id] )
+  end
 
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
 	def setup 
 		@category = Category.create(name: "sports")
+		@user = User.create(username: 'jean', email: "jean@123.com", password: "1234", admin: true)
 	end
 
 	test "shoul get categories index" do 
@@ -12,6 +13,8 @@ class CategoriesControllerTest < ActionController::TestCase
 	end
 
 	test "shoul get new" do 
+		#esta es la forma de simular la session en el controller - 
+		session[:user_id] = @user.id 
 		get :new
 		assert_response :success
 	end
@@ -19,6 +22,13 @@ class CategoriesControllerTest < ActionController::TestCase
 	test "shoul get show" do 
 		get(:show, {'id' => @category.id}) 
 		assert_response :success
+	end
+
+	test "should redirect create when admin not logged in" do
+		assert_no_difference 'Category.count' do
+			post :create, category: { name: "sports" }
+		end	
+		assert_redirected_to categories_path
 	end
 
 end
